af1547df14933b64cea52c5431a71a5bab07f275
Anshul Pandey <anshulpandey2@outlook.com>
2021-07-03 03:00:02 +0530

    Update wsgi

--- core/wsgi.py
+++ core/wsgi.py
@@ -10,9 +10,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
-
-from app.models import Data
-
-mydata = Data()
-mydata.myList = [99,98,97,98,98,98,99,100,104,100,99,100]
-mydata.save()
